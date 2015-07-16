191bb41f11d2a44c8006851b45f011b6dbcb07c1
toweave <lee@Toweave.com>
2015-07-16 23:26:50 +0800

    add js learn

---
+++ js/demo-liz.js
@@ -0,0 +1,39 @@
+/*!
+ * Toweave
+ * Copyright 2015 Liz1 lee
+ * Licensed under the Liz1 Lee
+ */
+
+
+/* $(function(){
+ 	$("#input").click(function(){
+ 		alert("this is a JS");
+ 	});
+ });
+
+ $(function(){
+ 	$("#box").css("color","red");
+ });
+
+ jQuery(function(){
+ 	jQuery("#box").css("color","blue");
+ })
+
+ alert($===jQuery);
+ alert($);
+
+ $().css("color","black");
+
+ $(document).ready()
+ $(function(){
+ 	alert("1");
+ });
+*/
+
+
+ $(function(){
+ 	alert($("#box").get(0));
+ });
+
+ jQuery,noConflict();
+ var $$=jQuery;
