e743e1824b53ff3563e5a0bfeb98a21f6e3af6fa
liz <liz@playab.net>
2015-07-22 18:27:08 +0800

    add selector

---
+++ js/demo-selector.js
@@ -0,0 +1,26 @@
+
+// $(function(){
+//   $(".liz[id]").addClass("test");
+// });
+
+// $("input[name='newsletter']").attr("checked", true);
+// $("input[name!='newsletter']").attr("checked", true);
+// $("input[name^='news']").attr("checked", true);
+// $("input[name$='letter']").attr("checked", true);
+// $("input[name*='let']").attr("checked", true);
+//$("input[id][name*='man']").addClass("focused");
+// $(".liz ul li:nth-child(3n+1)").addClass("test");
+// $(".liz ul li:first-child").addClass("test");
+// $("ul li:only-child").addClass("test");
+// $(":password").addClass("test");
+// $(":radio").attr("checked", true);
+// $(":checkbox").attr("checked", true);
+// $(":submit").addClass("test");
+// $(":image").addClass("test");
+// $(":reset").addClass("test");
+// $(":button").addClass("test");
+// $(":file").addClass("test");
+// $("input:enabled").addClass("test");
+// $("input:disabled").addClass("test");
+// $("input:checked").attr("checked", false);
+// $("select option:selected").attr("selected", false);
