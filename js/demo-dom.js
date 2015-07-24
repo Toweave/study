190905a132135baa0c75f44e68457f556a25ebd3
liz <liz@playab.net>
2015-07-24 18:42:25 +0800

    add exce

--- js/demo-dom.js
+++ js/demo-dom.js
@@ -1,4 +1,85 @@
-$(function(){
-	var box = $("<div id='box'>节点 </div> ");
-	$(".liz p").append(box);
-});
+// $(function(){
+// 	var box = $("<div id='box'>节点 </div> ");
+// 	$(".liz p").append(box);
+// });
+// $(".liz p").append("<b>Hello</b>");
+// $(".liz p").appendTo(".liz div");
+// $(".liz p").prepend("<b>Hello</b>");
+// $(".liz p").prepend( $(".foo")[0] );
+// $(".liz b").prependTo("#foo");
+// $(".liz p").after("<b>Hello Liz</b>");
+// $(".liz p").after( $("#foo")[0] );
+// $("p").before( $("#foo")[0] );
+// $(".liz p").insertAfter("#foo");
+// $(".liz p").insertBefore("#foo");
+// $('.inner').wrap(function() {
+//   return '<div class="' + $(this).text() + '" />';
+// });
+// $(".liz div").wrapInner("<b></b>");
+// $('.inner').wrapInner(function() {
+//   return '<div class="' + $(this).text() + '" />';
+// });
+// $(".liz p").replaceWith("<b>Paragraph. </b>");
+// $('.third').replaceWith($('.first'));
+// $(".btn-01").click(function(){
+//   $(this).clone(true).insertAfter(this);
+// });
+ // $(".liz div").click(function() {
+ //    $(this).css({
+ //      width: function(index, value) {
+ //        return parseFloat(value) * 1.2;
+ //      }, 
+ //      height: function(index, value) {
+ //        return parseFloat(value) * 1.2;
+ //      }
+ //    });
+ //  });
+// var p = $(".liz div");
+// var offset = p.offset();
+// p.html( "left: " + offset.left + ", top: " + offset.top );
+// $(".liz p:last").offset({ top: 110, left: 100 });
+// var p = $(".bottom");
+// $("p:last").text( "scrollTop:" + p.scrollTop() );
+// alert($(".bottom").scrollTop(300));
+// $(function(){
+//   $("body").click(function(){
+//     $("body").scrollTop(300);
+//   });
+// });
+// var p = $(".liz p:first");
+// $(".liz p:first").text( "scrollLeft:" + p.scrollLeft() );
+
+// $(".liz button").click(function(){
+//     $("p").height(function(n,value){
+//     return value+10;
+//     });
+//   });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- js/demo-selector.js
+++ js/demo-selector.js
@@ -24,3 +24,48 @@
 // $("input:disabled").addClass("test");
 // $("input:checked").attr("checked", false);
 // $("select option:selected").attr("selected", false);
+
+
+// $(".liz p").find("span").append("<b>This is exce.</b>");
+// $(".liz p").next().append("<b>This is exce.</b>");
+
+// $(".liz p").next(".select").css("background-color","#eee");
+// $(".liz p").nextAll().css("background-color","#eee");
+// $(".liz p").nextUntil('span').css("background-color","#eee");
+// $('#term-2').nextUntil('dt').css('background-color', 'red');
+// var term3 = document.getElementById("term-3");
+// $("#term-1").nextUntil(term3, "dd").css("color", "green");
+// var til = $(".liz p");
+// $(".liz p").nextUntil(til, "span").css("color", "green");
+// $("span").parents(".liz").css("color", "green");
+// $('.liz li.item-a').parentsUntil('.level-1')
+//     .css('background-color', 'red');
+// $(".liz p").prev().css('background-color', 'red');
+
+// $(".liz p").prev(".selected").css('background-color', 'red');
+// $(".liz div:last").prevAll().addClass("before");
+// $('#term-2').prevUntil('dt').css('background-color', 'red');
+// $(".liz dd").siblings().css('background-color', 'red');
+// $(".liz div").siblings(".selected").css('background-color', 'red');
+
+// $("test1").add("<span>Again 123</span>");
+// $("p").add(document.getElementById("a"));
+// $(".liz").find("p").andSelf().addClass("border");
+// $(".liz p").contents().not("[nodeType=1]").wrap("<b/>");
+// $("iframe").contents().find("body")
+//   .append("I'm in an iframe!");
+$("liz p").find("span").end()
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 
